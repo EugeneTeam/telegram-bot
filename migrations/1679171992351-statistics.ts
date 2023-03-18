38f0e64b16c3a8b21aa6312934d2c2c9b149f50d
EugeneTeam <wtf.evgesha@gmail.com>
2023-03-18 22:58:15 +0200

    ✅fix migration - statistics

--- migrations/1679171992351-statistics.ts
+++ migrations/1679171992351-statistics.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export class words1679171992351 implements MigrationInterface {
+export class statistics1679171992351 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
